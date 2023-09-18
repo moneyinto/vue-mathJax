3f823a5c299632011a9371d9f53b261e850dc0f2
moneyinto <moneyinto@moneyintodeiMac.local>
2023-09-18 15:02:38 +0800

    初始化循环使用插件导致的批量初始化存在失败问题

--- src/plugins/mathjax.ts
+++ src/plugins/mathjax.ts
@@ -1,29 +1,44 @@
+// 处理批量初始化问题
+const callbackArr: (() => void)[] = [];
+let isInit = true;
+
 export const initMathJax = (config = {}, callback?: () => void) => {
     if (window.MathJax) return callback && callback();
-    const script = document.createElement("script");
-    script.src = "/mathjax/3.2.2/tex-svg-full.js";
-    script.async = true;
-    document.head.appendChild(script);
-    // 没有找到好的配置解决办法，这里直接在localstorage里存入配置值
-    // localStorage.setItem("MathJax-Menu-Settings", '{"renderer":"svg"}');
-    const defaultConfig = {
-        loader: { load: ["[tex]/unicode", "[tex]/mhchem"] },
-        tex: { packages: { "[+]": ["unicode", "mhchem"] } },
-        options: {
-            enableMenu: false,
-            menuOptions: {
-                settings: {
-                    renderer: "svg"
+    if (isInit) {
+        isInit = false;
+        const script = document.createElement("script");
+        script.src = "/mathjax/3.2.2/tex-svg-full.js";
+        script.async = true;
+        document.head.appendChild(script);
+        // 没有找到好的配置解决办法，这里直接在localstorage里存入配置值
+        // localStorage.setItem("MathJax-Menu-Settings", '{"renderer":"svg"}');
+        const defaultConfig = {
+            loader: { load: ["[tex]/unicode", "[tex]/mhchem"] },
+            tex: { packages: { "[+]": ["unicode", "mhchem"] } },
+            options: {
+                enableMenu: false,
+                menuOptions: {
+                    settings: {
+                        renderer: "svg"
+                    }
+                }
+            },
+            startup: {
+                pageReady: () => {
+                    callback && callback();
+
+                    if (callbackArr.length > 0) {
+                        for (const c of callbackArr) {
+                            c();
+                        }
+                    }
                 }
             }
-        },
-        startup: {
-            pageReady: () => {
-                callback && callback();
-            }
-        }
-    };
+        };
 
-    const mergeConfig = Object.assign({}, defaultConfig, config);
-    window.MathJax = mergeConfig;
+        const mergeConfig = Object.assign({}, defaultConfig, config);
+        window.MathJax = mergeConfig;
+    } else {
+        if (callback) callbackArr.push(callback);
+    }
 };
